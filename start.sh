1f0d8863d491993a23ef369060f77ace66bf6f1a
munum <michael.chan.is@gmail.com>
2013-06-07 16:38:05 +0100

    set 9 instances

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ssh -o StrictHostKeyChecking=no -i askbot-test-ec2.pem -f -N -L 9312:localhost:9312 ubuntu@107.21.228.25 
 sleep 3
-newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" -w 3
+newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" -w 9
 
