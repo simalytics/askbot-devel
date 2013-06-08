e8753626af4d14a8656e4911aff02c2d2e711a46
munum <michael.chan.is@gmail.com>
2013-06-08 19:20:37 +0100

    fix max-requests

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ssh -o StrictHostKeyChecking=no -i askbot-test-ec2.pem -f -N -L 9312:localhost:9312 ubuntu@107.21.228.25 
 sleep 3
-newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" --max_requests=500 -w 4 --preload
+newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" --max-requests=500 -w 4 --preload
