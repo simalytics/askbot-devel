1152c93b3c19416657cfb9e3bdc3ffb5d1123f48
munum <michael.chan.is@gmail.com>
2013-06-08 16:27:58 +0100

    restore start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 ssh -o StrictHostKeyChecking=no -i askbot-test-ec2.pem -f -N -L 9312:localhost:9312 ubuntu@107.21.228.25 
 sleep 3
-#newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" -w 4
-#newrelic-admin run-program python manage.py runserver "0.0.0.0:$PORT" 
-python manage.py runserver "0.0.0.0:$PORT" 
-
+newrelic-admin run-program python manage.py run_gunicorn "0.0.0.0:$PORT" -w 4
